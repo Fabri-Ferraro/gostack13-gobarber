2475853af6c2a716c80bc1bbb7b09a521e2da812
Fabri <lucasfabriferraro@gmail.com>
2020-11-11 00:30:31 -0300

    mobile, server e web

---
+++ server/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,71 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours, isAfter } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailability {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const currentDate = new Date(Date.now());
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      const compareDate = new Date(year, month - 1, day, hour);
+
+      // 2020-05-20 08:00:00
+      // 2020-05-20 10:00:00
+      // 2020-05-20 11:00:00
+      // 2020-05-20 12:00:00
+
+      return {
+        hour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailability;
